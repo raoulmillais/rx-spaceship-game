b5f613a25f3c6d32c6351eb1a477e827b8024051
Raoul Millais <raoul@raoulmillais.com>
2016-05-04 16:48:09 +0100

    Typescript infrastructure

--- index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-
-app.use('/build', express.static(__dirname + '/build'));
-app.get('/', (req, res) =>
-    res.status(200).sendFile(path.join(__dirname, './index.html')));
-app.listen(process.env.PORT || 8080);
